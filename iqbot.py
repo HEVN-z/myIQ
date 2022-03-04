9ff9ac0d3f62e9efbf79a633dd59c750368266f3
HEVNz <e-exe-e@live.com>
2022-03-04 12:55:12 +0700

    update

--- iqbot.py
+++ iqbot.py
@@ -1,14 +1,35 @@
+# import api
+
 import time
 from iqoptionapi.stable_api import IQ_Option
 
-from indic_psar import PSAR
+# import class
 
+from indic_psar import PSAR
+from mm import MoneyManager
+
+# Stand-in
+
+indic_psar = PSAR()
+mm = MoneyManager()
+
+# login connection
 
 email = "dummy.esper@gmail.com"
 password = "12651265exe"
 
 bot = IQ_Option(email,password)
 bot.connect()
+while True:
+    if bot.check_connect():
+        print("Connected")
+        break
+    else:
+        print("Not Connected")
+        break
+
+# Asset Management
+
 ALL_Asset= bot.get_all_open_time()
 #print("ALL_Asset:", ALL_Asset)
 #print("ALL_Asset[0]:", ALL_Asset[0])
@@ -20,20 +41,15 @@ goal = "EURUSD"
 #print("candles:", candles)
 #indicator = bot.get_technical_indicators(goal)
 #print("\"indicator:\" = ", indicator)
-indic_psar = PSAR()
 
-while True:
-    if bot.check_connect():
-        print("Connected")
-        break
-    else:
-        print("Not Connected")
-        break
+print(mm.MoneySplit(bot.get_balance())[0])
 
 balance_type = "PRACTICE"
 bot.change_balance(balance_type)
 print("Balance: ",bot.get_balance())
 amount = float(input("amount: "))
+
+# Signal Command
 
 while True:
     signal = input("Command: ")
@@ -41,6 +57,7 @@ while True:
         check, id = bot.buy(amount,"EURUSD","call",1)
         if check:
             print("!buy!")
+            print(bot.check_win_v4(id)[0],"You got",round(bot.check_win_v4(id)[1],2))
         else:
             print("buy fail")
         #bot.buy_digital_spot("EURUSD",amount,"call",1)
@@ -48,6 +65,7 @@ while True:
         check, id = bot.buy(amount,"EURUSD","put",1)
         if check:
             print("!sell!")
+            print(bot.check_win_v4(id)[0],"You got",round(bot.check_win_v4(id)[1],2))
         else:
             print("sell fail")
         #bot.buy_digital_spot("EURUSD",amount,"put",1)
--- mm.py
+++ mm.py
@@ -1,13 +1,12 @@
-ballance = 100
-money = ballance
-pocket1 = (money/3).__floor__()
-pocket2 = (money/3).__floor__()
-pocket3 = (money/3).__floor__()
-print("pocket 1 =",pocket1,"pocket 2 =",pocket2,"pocket 3 =",pocket3)
-while(True):
-    if pocket1 == pocket2 == pocket3:
-        print("All pocket are equal")
-        break
-    else:
-        print("All pocket are not equal")
-        break
+class MoneyManager:
+    def __init__(self):
+        pass
+    def MoneySplit(self,balance):
+        self.pocket1 = (balance/3).__floor__()
+        self.pocket2 = (balance/3).__floor__()
+        self.pocket3 = (balance/3).__floor__()
+        return self.pocket1, self.pocket2, self.pocket3
+
+    def martingale():
+        print("martingale")
+        pass
