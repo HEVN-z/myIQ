6d4e4b387f2b06b0b529083cc69085bef7abc88b
HEVNz <e-exe-e@live.com>
2022-03-06 15:34:24 +0700

    Update

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-Active = input("Currency pair : ")
-if Active == "" or Active == NULL:
-    Active = "EURUSD"
-    Active = Active.upper()
-    print("Default currency pair =",Active)
-else:
-    Active = Active.upper()
-    print("Your currency pair is",Active)
