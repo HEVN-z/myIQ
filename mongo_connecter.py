4735ba66f724cf6bcabf9b4930448889a8f18451
HEVNz <e-exe-e@live.com>
2022-05-26 00:30:37 +0700

    g

--- mongo_connecter.py
+++ mongo_connecter.py
@@ -11,3 +11,19 @@ signal_db = client.signal
 user_db = client.user
 mm_db = client.mm
 history_db = client.history
+
+def get_signal_db_TIA(market):
+    return signal_db.tech_indicator.find_one({'market':market})['signal']
+
+def set_signal_db_TIA(market,signal):
+    # TIA = Technical Indicator Analysis
+    global signal_db
+    tia = signal_db.tech_indicator
+    if tia.find_one({"market":market}) is None:
+        tia.insert_one({"market":market,"signal":signal})
+    else:
+        tia.update_one({"market":market},{"$set":{"signal":signal}})
+
+
+set_signal_db_TIA('EURUSD','call')
+print(get_signal_db_TIA('EURUSD'))
--- mongo_db.py
+++ mongo_db.py
@@ -11,9 +11,9 @@ db = client.test
 print(db)
 start = time.time()
 db = client['test']
-collection = db['test']
-history = collection['history']
-# history.insert_one({"_id":1,"name": "Alice"})
+# collection = db['test']
+history = db['history']
+history.insert_one({"_id":1,"name": "Alice"})
 history.update_one({"_id":1},{"$set":{"no": "126354"}})
 history.update_one({"_id":1},{"$set":{"name": "Alice"}})
 # start = time.time()
