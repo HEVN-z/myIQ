4b86793de731dc4239df027e24e7ddafcb018212
HEVNz <e-exe-e@live.com>
2022-05-25 22:54:08 +0700

    g

---
+++ mongo_db.py
@@ -0,0 +1,27 @@
+import pymongo
+import time
+import os
+import dotenv
+dotenv.load_dotenv()
+
+start = time.time()
+
+client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+db = client.test
+print(db)
+start = time.time()
+db = client['test']
+collection = db['test']
+history = collection['history']
+# history.insert_one({"_id":1,"name": "Alice"})
+history.update_one({"_id":1},{"$set":{"no": "126354"}})
+history.update_one({"_id":1},{"$set":{"name": "Alice"}})
+# start = time.time()
+y = history.find_one({"_id": 1})
+name = y['name']
+no = y['no']
+print(name)
+print(no)
+end = time.time()
+print(end-start)
+
