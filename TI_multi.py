e194b494b6d21c6ba6ab8613df7261d79816b9ac
HEVNz <e-exe-e@live.com>
2022-06-01 09:26:47 +0700

    g

--- TI_multi.py
+++ TI_multi.py
@@ -4,27 +4,27 @@
 
 import json
 
-
 ############################################################################################
 ## Load files
 ############################################################################################
 
-json_file = json.load(open("TI_multi.json"))
+file = "TI_multi.json"
+json_file = json.load(open(file))
 
 ###############################################################################################
 ## Function
 ###############################################################################################
 
 def get_enable(active):
-    json_file = json.load(open("TI_multi.json",'r'))
+    json_file = json.load(open(file,'r'))
     return json_file[active]['enable']
 
 def get_data(active,time):
-    json_file = json.load(open("TI_multi.json",'r'))
+    json_file = json.load(open(file,'r'))
     return json_file[active]['data'][time]
 
 def get_percent(active):
-    json_file = json.load(open("TI_multi.json",'r'))
+    json_file = json.load(open(file,'r'))
     return json_file[active]['data']['percent']
 
 ############################################################################################
@@ -33,15 +33,16 @@ def get_percent(active):
 
 def set_update_enable(active,new_enable):
     json_file[active] = new_enable
-    json.dump(json_file, open("TI_multi.json", "w"),indent=4)
+    json.dump(json_file, open(file, "w"),indent=4)
 
 def set_data(active,one,five,fifteen,sixty):
     json_file[active]['data']['one'] = one
     json_file[active]['data']['five'] = five
     json_file[active]['data']['fifteen'] = fifteen
     json_file[active]['data']['hour'] = sixty
-    json.dump(json_file, open("TI_multi.json", "w"),indent=4)
+    json.dump(json_file, open(file, "w"),indent=4)
 
 def set_percent(active,percent):
     json_file[active]['data']['percent'] = percent
-    json.dump(json_file, open("TI_multi.json", "w"),indent=4)
+    json.dump(json_file, open(file, "w"),indent=4)
+
